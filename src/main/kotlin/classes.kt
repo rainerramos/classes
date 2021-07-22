2490c4f4151ad18c8a3c7725f1acdd36509035ae
Rainer Ramos <rainermr89@hotmail.com>
2021-07-22 13:32:35 -0300

    Classes

--- src/main/kotlin/classes.kt
+++ src/main/kotlin/classes.kt
@@ -1,3 +1,19 @@
+class Pessoa1
+
+class Pessoa2 (var nome: String, var anoNascimento: Int)
+
+class Pessoa3 (var nome: String, var anoNascimento: Int) {
+    fun saudacao(){
+        println("olá, meu nome é $nome")
+    }
+}
+
 fun main(){
-    
+
+    val pessoa: Pessoa3 = Pessoa3(nome = "Rainer", anoNascimento = 32)
+    println(pessoa.nome)
+    println(pessoa.anoNascimento)
+
+    pessoa.saudacao()
+
 }
