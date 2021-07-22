baae6f406a74b6959fdbbe431b4bcec604d3e05a
Rainer Ramos <rainermr89@hotmail.com>
2021-07-22 14:21:46 -0300

    Field

--- src/main/kotlin/classes.kt
+++ src/main/kotlin/classes.kt
@@ -1,4 +1,18 @@
 class Maquina (var marca: String) {
 
+    var nucleos: Int = 0
+    get(){
+        println("Get foi chamado")
+        return field
+    }
+    set(value){
+        println("set foi chamado")
+        field = value
+    }
 
+}
+fun main(){
+    var m = Maquina("xpto")
+    println(m.nucleos)
+    m.nucleos = 10
 }
